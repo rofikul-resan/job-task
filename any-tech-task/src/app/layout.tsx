8af3ce55474ef01e072be4c84619906b941ca6a2
rofikul-resan <resan6231@gmail.com>
2025-01-30 23:43:49 +0600

    any tech job task fille color font and theme set

---
+++ any-tech-task/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter, Montserrat } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Home Page | AnyTech",
+  description: "Develop by Rofikul Islam Resan",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${montserrat.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
