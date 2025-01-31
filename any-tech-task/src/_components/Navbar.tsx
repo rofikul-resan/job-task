14d99c94908ad88d2a69472b10573f1a8c447e4a
rofikul-resan <resan6231@gmail.com>
2025-01-31 23:12:01 +0600

    navbar complite

--- any-tech-task/src/_components/Navbar.tsx
+++ any-tech-task/src/_components/Navbar.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import React from "react";
 import logo from "@/asset/logo.svg";
 import Button from "@/_lib/Button";
+import { TbWorld } from "react-icons/tb";
+import { FaAngleDown, FaAngleRight } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="bg-black text-white max-lg:py-8 top-0 lg:absolute w-full lg:pt-[17px] lg:pb-[25px] text-res-body-p1 lg:text-body-p1 z-[9999] container flex items-center flex-wrap justify-between  ">
+    <nav className=" text-white max-lg:py-8 top-0 lg:absolute w-full lg:pt-[17px] lg:pb-[25px] z-[9999] container flex items-center flex-wrap justify-between  ">
       <div>
         <Image
           src={logo}
@@ -15,25 +17,47 @@ const Navbar = () => {
           className="h-7 w-auto"
         />
       </div>
-      <ul className=" flex gap-4 items-center">
-        <li>Solutions</li>
-        <li>Services</li>
-        <li>About Us</li>
-        <li>
+      <ul className=" flex items-center mb-1">
+        <li className="relative px-6 py-3 flex items-center justify-between gap-2 group transition-all  ">
+          <span>Solutions</span> <FaAngleDown className="mt-1" />
+          <ul className="absolute hidden opacity-0 top-12 rounded  left-6 bg-white w-[240px] h-full text-black group-hover:block group-hover:opacity-100 duration-300">
+            <li className="py-3 px-4 w-[240px] border-b-[0.5px] hover:text-primary  border-gray-200">
+              AnyCaaS
+            </li>
+            <li className="py-3 px-4 w-[240px] border-b-[0.5px] hover:text-primary  border-gray-200">
+              AnyBaaS
+            </li>
+            <li className="py-3 px-4 w-[240px]  hover:text-primary  ">
+              AnyPaaS
+            </li>
+          </ul>
+        </li>
+        <li className="px-6 py-3 hover:border-b border-white">Services</li>
+        <li className="px-6 py-3 hover:border-b border-white">About Us</li>
+        <li className="px-6 py-3 ">
           <Button
             style={{ borderRadius: "9999px", textTransform: "uppercase" }}
-            className="border-white border "
+            className="border-white border  h-full "
           >
+            <span>
+              <TbWorld />
+            </span>
             <span className="w-full">en</span>
+            <span>
+              <FaAngleDown />
+            </span>
           </Button>
         </li>
       </ul>
       <div>
         <Button
-          className="bg-secondary
+          className="border border-white bg-transparent hover:bg-white hover:text-primary transition-all shadow-md gap-2 group  
         "
         >
-          Contact Us
+          <span>Contact Us</span>{" "}
+          <span className="group-hover:translate-x-2 duration-500 delay-150">
+            <FaAngleRight />
+          </span>
         </Button>
       </div>
     </nav>
--- any-tech-task/src/app/page.tsx
+++ any-tech-task/src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/_components/Navbar";
 export default function Home() {
   return (
     <main>
-      <div className="max-w-7xl mx-auto sticky top-0 ">
+      <div className="max-w-[1200px] mx-auto sticky top-0 ">
         <Navbar />
       </div>
     </main>
