d501f826272005e588139252bf13320fd6b6711b
rofikul-resan <resan6231@gmail.com>
2025-02-01 23:24:12 +0600

    fatch problem in svg

--- any-tech-task/src/app/(home)/_components/HeroSection.tsx
+++ any-tech-task/src/app/(home)/_components/HeroSection.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import React from "react";
 import heroImg from "@/asset/herobg.jpg";
+import { ReactComponent as HeroSvg1 } from "@/asset/backgrounds/WaveLinesDesktop1.svg";
+import { ReactComponent as HeroSvg2 } from "@/asset/backgrounds/WaveLinesDesktop2.svg";
+import { ReactComponent as HeroSvg3 } from "@/asset/backgrounds/WaveLinesDesktop3.svg";
+import { ReactComponent as HeroSvg4 } from "@/asset/backgrounds/WaveLinesDesktop4.svg";
 
 const HeroSection = () => {
   return (
@@ -8,7 +12,7 @@ const HeroSection = () => {
       {/* background image  */}
       <div className="lg:h-[max(620px,_calc(92vh-49px))] h-auto relative w-full text-white hero-bg-clip overflow-hidden pt-[30%] md:pt-[20%] lg:pt-0 max-lg:pb-[15%] hero-bg-gradient -z-[99999]">
         {/* hero image  */}
-        <div className="absolute hero-bg-image-clip h-full hidden left-1/2 lg:block lg:w-full pointer-events-none svelte-1lc7tvy top-0 xl:left-[35%] xl:w-[65%]">
+        <div className="absolute hero-bg-image-clip h-full hidden left-1/2 lg:block lg:w-full pointer-events-none  top-0 xl:left-[35%] xl:w-[65%]">
           <Image
             src={heroImg}
             alt="hero bg"
@@ -16,6 +20,12 @@ const HeroSection = () => {
             width={600}
             className="object-cover w-[105%] h-[105%]"
           />
+        </div>
+        <div className="absolute inset-0 z-10">
+          <HeroSvg1 />
+          <HeroSvg2 />
+          <HeroSvg3 />
+          <HeroSvg4 />
         </div>
       </div>
     </div>
